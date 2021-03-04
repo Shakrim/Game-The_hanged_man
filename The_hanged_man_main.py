fdda935f5f296553c161ec874efe650d63e71f1e
Pavel Vanacek <pavelvanacek@gmail.com>
2021-03-04 14:57:57 +0100

    I translate the my original code into english.

--- The_hanged_man_main.py
+++ The_hanged_man_main.py
@@ -3,27 +3,27 @@ import os
 from random import choice
 import The_hanged_man_scheme
 
-slovo = choice(["obesenec", "auto", "autobus", "pondeli"])
-tajenka = len(slovo) * ["_"]
-zivoty = 7
-hra_probiha = True
-while hra_probiha and zivoty > 0:
+guessed_word = choice(["sex", "sibling", "cholesterol", "monday","lesbian"])
+puzzle = len(guessed_word) * ["_"]
+life = 7
+game_on = True
+while game_on and life > 0:
     os.system("cls")
-    print(The_hanged_man_scheme.hangman[7 - zivoty])
-    print(f"Tajenka: {tajenka}, {zivoty}")
-    hadani = input("Zadej pismeno nebo slovo: ").lower()
-    if slovo == hadani:
-        hra_probiha = False
-    elif len(hadani) == 1 and hadani in slovo:
-        for index, pismeno in enumerate(slovo):
-            if pismeno == hadani:
-                tajenka[index] = pismeno
-        hra_probiha = False if "_" not in tajenka else True
+    print(The_hanged_man_scheme.hangman[7 - life])
+    print(f"Puzzle: {puzzle}, {life}")
+    guessing = input("Enter letter or guessed word: ").lower()
+    if guessed_word == guessing:
+        game_on = False
+    elif len(guessing) == 1 and guessing in guessed_word:
+        for index, letter in enumerate(guessed_word):
+            if letter == guessing:
+                puzzle[index] = letter
+        game_on = False if "_" not in puzzle else True
     else:
-        zivoty -= 1
-if not hra_probiha:
-    print("Uhod jsi slovo!")
+        life -= 1
+if not game_on:
+    print("You got it!")
 else:
     os.system("cls")
-    print(The_hanged_man_scheme.hangman[7 - zivoty])
-    print(f"PROHRAL JSI, TAJNE SLOVO BYLO *{slovo}*")
+    print(The_hanged_man_scheme.hangman[7 - life])
+    print(f"You failed, the quessed word was *{guessed_word}*")
