3f2581ddf57191aade5a1b3e736e794b44be809f
Pavel Vanacek <pavelvanacek@gmail.com>
2021-04-30 10:47:31 +0200

    I add new function for checking the status of game and lives of user.

--- The_hanged_man_main.py
+++ The_hanged_man_main.py
@@ -3,17 +3,27 @@ import os
 from random import choice
 import The_hanged_man_scheme
 
+def actual_game_Status(lives:int, puzzle:list, game_on:bool)->None:
+    os.system("cls")
+    status = f"Puzzle: {' '.join(puzzle)} Lives left: {lives}"
+    print(status, The_hanged_man_scheme.hangman[7 - lives], sep="\n")
+    if not game_on and lives:
+        print("You won!")
+
+    elif not lives:
+        print("You lost!")
+
 guessed_word = choice(["sex", "sibling", "cholesterol", "monday","lesbian"])
 puzzle = len(guessed_word) * ["_"]
-life = 7
+lives = 7
 game_on = True
 
 
-while game_on and life > 0:
-    os.system("cls")
-    print(The_hanged_man_scheme.hangman[7 - life])
-    print(f"Puzzle: {' '.join(puzzle)} Lifes left: {life}")
+
+while game_on and lives > 0:
+    actual_game_Status(lives, puzzle, game_on)
     guessing = input("Enter letter or guessed word: ").lower()
+
     if guessed_word == guessing:
         game_on = False
     elif len(guessing) == 1 and guessing in guessed_word:
@@ -22,10 +32,10 @@ while game_on and life > 0:
                 puzzle[index] = letter
         game_on = False if "_" not in puzzle else True
     else:
-        life -= 1
-if not game_on:
-    print("You got it!")
-else:
-    os.system("cls")
-    print(The_hanged_man_scheme.hangman[7 - life])
-    print(f"You failed, the quessed word was *{guessed_word}*")
+        lives -= 1
+
+actual_game_Status(lives, puzzle, game_on)
+
+
+
+
